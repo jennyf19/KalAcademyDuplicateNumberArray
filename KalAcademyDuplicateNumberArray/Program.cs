ad814b4456c956b15c235344b1c9c10c3ed2bfda
jennyf19 <ferriesj@gmail.com>
2016-09-17 16:38:52 -0700

    work in progress

--- KalAcademyDuplicateNumberArray/Program.cs
+++ KalAcademyDuplicateNumberArray/Program.cs
@@ -10,6 +10,21 @@ namespace KalAcademyDuplicateNumberArray
     {
         static void Main(string[] args)
         {
+            int[] myArray1 = { 4, 5, 7, 6, 4, 12 };
+            int sum1 = myArray1.Sum();
+            int[] myArray2 = new int[5];
+
+            Array.Copy(myArray1, 0, myArray2, 0, 5);
+
+            int sum2 = myArray2.Sum();
+            int duplicate = sum1 - sum2;
+
+            Console.WriteLine("The duplicate is " + duplicate);
+            Console.ReadLine();
+
         }
+
+
     }
+
 }
